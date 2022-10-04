0312b58134a99286408edd11475ff9b9b4813196
thoziz <riq.aziz@gmail.com>
2022-10-04 13:31:22 +0700

    endpoint

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/me', function(){
+    return [
+        'Name' => 'Thoriq',
+        'Kelas' => 'XII RPL 0',
+        'NIS' => '123456',
+    ];
+});
